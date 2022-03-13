471a3367b12074d7f019d916545a68a50c19977c
kkap008 <yoonsik.jeong.1993@gmail.com>
2022-03-14 07:38:14 +0900

    Update

--- jsTask/220311/carouselSlider/component/carouselSlider/carouselSlider.js
+++ jsTask/220311/carouselSlider/component/carouselSlider/carouselSlider.js
@@ -1,18 +1,35 @@
-class carouselSlider extends HTMLElement {
-  constructor() {
-    super();
-    console.log(this);
-  }
-}
-
-const $carousel_slider = document.querySelector(
+const carouselSlider = document.querySelector(
   "[data-include='carousel-slider']"
 );
 
-const carouselslider_component = document.createElement(
-  "carouselslider-component"
+const xmlhttp = new XMLHttpRequest();
+
+xmlhttp.addEventListener("readystatechange", (event) => {
+  const xmlHttp = event.target;
+  const xmlHttpReadyState = event.target.readyState;
+  const xmlHttpStatus = event.target.status;
+
+  if (xmlHttpReadyState === 4 && xmlHttpStatus === 200) {
+    const domParser = new DOMParser();
+    const responseText = xmlHttp.responseText;
+    const componentHtml = domParser.parseFromString(responseText, "text/html");
+    const style = componentHtml.querySelector("style");
+    const componentElement = componentHtml.body.firstElementChild;
+    const textType = 3;
+    const fragment = new DocumentFragment();
+    fragment.appendChild(style);
+    for (let i = 0; i < componentElement.childNodes.length; ++i) {
+      const child = componentElement.childNodes[i];
+      if (child.nodeType !== textType) {
+        fragment.appendChild(componentElement.childNodes[i]);
+      }
+    }
+    carouselSlider.appendChild(fragment);
+  }
+});
+
+xmlhttp.open(
+  "GET",
+  `${location.origin}/jsTask/220311/carouselSlider/component/carouselSlider/carouselSlider.html`
 );
-
-const $body = document.querySelector("body");
-
-$body.appendChild(carouselslider_component);
+xmlhttp.send();
