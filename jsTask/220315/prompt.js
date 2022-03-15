18f93d48e3d1b2b325bacddbef19deb8d6ace644
kkap008 <yoonsik.jeong.1993@gmail.com>
2022-03-16 00:57:59 +0900

    Update

--- jsTask/220315/prompt.js
+++ jsTask/220315/prompt.js
@@ -1,36 +1,72 @@
 "use strict";
 
-(function () {
-  function promptEvent() {
-    const local = prompt("사는 지역은?") || undefined;
-    const user = prompt("이름은?") || undefined;
-    const $print = document.querySelector("[data-prompt='print']");
+class Print {
+  constructor() {
+    this.$btn = document.querySelector("[data-prompt='btn']");
+    this.$print = document.querySelector("[data-prompt='print']");
+  }
 
+  addEventListener(target, type, callBack) {
+    target.addEventListener(type, callBack);
+  }
+
+  check(local, user) {
     switch (local && user) {
       case undefined:
       case false:
-        if (!local && !user) {
-          $print.textContent = "이름과 주소를 입력해주세요";
-        } else if (!local) {
-          $print.textContent = "주소를 입력해주세요";
-        } else {
-          $print.textContent = "이름을 입력해주세요";
-        }
+        this.checkInput(local, user);
         break;
       default:
-        if (user.match(/[\d]+/)) {
-          $print.textContent = "이름은 영문,한글로 입력해주세요";
-          return;
-        } else {
-          $print.textContent = `${user
-            .match(/[a-zA-Z\s가-힣ㄱ-ㅎ]/g)
-            .join()}가 사는 지역은 ${local}입니다`;
-        }
-
+        this.matchName(local, user);
         break;
     }
   }
 
-  const $input = document.querySelector("[data-prompt='input']");
-  $input.addEventListener("click", promptEvent);
-})();
+  checkInput(local, user) {
+    if (!local && !user) {
+      this.$print.textContent = "이름과 주소를 입력해주세요";
+    } else if (!local) {
+      this.$print.textContent = "주소를 입력해주세요";
+    } else {
+      this.$print.textContent = "이름을 입력해주세요";
+    }
+  }
+
+  matchName(local, user) {
+    if (user.match(/[\d]+/)) {
+      this.$print.textContent = "이름은 영문,한글로 입력해주세요";
+      return;
+    } else {
+      if (!this.$print.firstChild) {
+        this.$name = document.createElement("span");
+        this.$address = document.createElement("span");
+        this.$print.append(
+          "이름 : ",
+          this.$name,
+          document.createElement("br"),
+          "거주 지역 : ",
+          this.$address
+        );
+      }
+
+      this.$name.textContent = user.match(/[a-zA-Z\s가-힣ㄱ-ㅎ]/g).join("");
+      this.$address.textContent = local;
+    }
+  }
+}
+
+class PrintEvent extends Print {
+  constructor() {
+    super();
+  }
+
+  printCallback = () => {
+    const local = prompt("사는 지역은?") || undefined;
+    const user = prompt("이름은?") || undefined;
+
+    this.check(local, user);
+  };
+}
+
+const print = new PrintEvent();
+print.addEventListener(print.$btn, "click", print.printCallback);
