468040d0876a30fe9ad27f0156536dd5a1f517d8
kkap008 <yoonsik.jeong.1993@gmail.com>
2022-03-15 18:15:26 +0900

    Update

--- jsTask/220315/tab_menu.js
+++ jsTask/220315/tab_menu.js
@@ -1,6 +1,7 @@
 "use stric";
 
 function template() {
+  // 스타일 정의
   const style = `
   <style>
         *{
@@ -31,6 +32,8 @@ function template() {
         }
     </style>
   `;
+
+  // 엘리먼트 정의
   const element = `
     <ul data-tab-menu="list">
         <li data-tab-menu="item">
@@ -64,63 +67,80 @@ function template() {
     </figure>
     `;
 
+  // 템플릿 엘리먼트 생성
   const template = document.createElement("template");
 
+  // 템플릿 엘리먼트에 "style","element" 추가 # innerHTML은 문자열을 자동으로 렌더링
   template.innerHTML = `${style} ${element}`;
 
+  // 템플릿 요소 반환
   return template;
 }
 
+// "tab-menu"에 사용할 클래스
 class TabMenu extends HTMLElement {
   constructor() {
+    // HTMLElement 객체에서 상속 받기
     super();
+
+    // 커스텀 엘리먼트 자손으로 쉐도우 돔 만들기
     this.attachShadow({ mode: "open" });
+
+    // "template" 함수에서 template 엘리먼트 값 받기
     this.template = template();
-    this.content = this.template.content;
-    this.init();
-    this.shadowRoot.appendChild(this.content);
+
+    // 쉐도우 돔 자손으로 template 추가
+    this.shadowRoot.appendChild(this.template.content);
   }
 
-  init() {
-    const $banner = this.content.querySelector("[data-tab-menu='banner']");
-    const src = this.content
-      .querySelector("[data-tab-menu='img']")
-      .getAttribute("src");
-
-    $banner.setAttribute("src", src);
-  }
-
+  // 쉐도우 돔에 추가될 때 호출
   connectedCallback() {
+    // function.html에서 스타일을 지정해야하지만
+    // 그냥 인라인으로 바로 지정
     this.style.cssText = `display: block;
         max-width: 1020px;
         margin: 0 auto;`;
+
+    // 쿼리를 위한 쉐도우 루트 가져오기
+    const shadowRoot = this.shadowRoot;
+
+    // 쉐도우 루트 객체에 탭 메뉴 속성 추가
+    Reflect.set(
+      shadowRoot,
+      "$tab_menu",
+      shadowRoot.querySelector("[data-tab-menu='list']")
+    );
+
+    // 탭 메뉴 객체에 배너 속성 추가
+    Reflect.set(
+      shadowRoot.$tab_menu,
+      "$banner",
+      shadowRoot.querySelector("[data-tab-menu='banner']")
+    );
+
+    // 탭 메뉴 중 첫번째 이미지 "src" 가져오기
+    const $firstImageSrc = shadowRoot.$tab_menu
+      .querySelector("[data-tab-menu='img']")
+      .getAttribute("src");
+
+    // 쉐도우 루트 객체를 이용한 배너 "src"를 초기값으로 셋팅
+    shadowRoot.$tab_menu.$banner.setAttribute("src", $firstImageSrc);
   }
 }
 
+// "tab-menu로 지정된 커스텀 엘리먼트에 "TabMenu" 클래스 인트턴스화
 customElements.define("tab-menu", TabMenu);
 
+// 쉐도우 돔은 캡슐화되어 있어서 "querySelector" 이후 "shadowRoot" 속성 호출
 const $TAB_MENU = document.querySelector("tab-menu").shadowRoot;
-// const $TAB_MENU_LIST = $TAB_MENU.querySelector("[data-tab-menu='list']");
 
-const test = $TAB_MENU.querySelector("[data-tab-menu='banner']");
-
-const $TAB_MENU_LIST = $TAB_MENU.querySelector("[data-tab-menu='list']");
-
-Reflect.set(
-  $TAB_MENU_LIST,
-  "banner",
-  $TAB_MENU.querySelector("[data-tab-menu='banner']")
-);
-
-console.dir($TAB_MENU_LIST);
-
-$TAB_MENU_LIST.addEventListener("click", (event) => {
+$TAB_MENU.$tab_menu.addEventListener("click", (event) => {
   const target = event.target;
   const currentTarget = event.currentTarget;
 
   switch (target.localName) {
     case "img":
-      currentTarget.banner.setAttribute("src", target.src);
+      currentTarget.$banner.setAttribute("src", target.src);
       break;
     default:
       return;
